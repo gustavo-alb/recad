9f99eb9996ff4912845329e0b574543d2f4d251a
udes <gustavohrq.alb@gmail.com>
2015-05-12 12:20:21 -0300

    Ambiente

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #Local.create(:nome=>"Polo Universitário Equador - Universidade Aberta do Brasil",:codigo=>"AP01054860")
-Usuario.create(:inep=>"AP01054860",:password=>"AP01054860",:password_confirmation=>"AP01054860",:local=>Local.find(:codigo=>"AP01054860"))
+Usuario.create(:inep=>"AP01054860",:password=>"AP01054860",:password_confirmation=>"AP01054860",:local=>Local.find_by(:codigo=>"AP01054860"))
