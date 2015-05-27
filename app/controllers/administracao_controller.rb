66e256e137dacdd63027b3335ca45233ebcd42cc
udes <gustavohrq.alb@gmail.com>
2015-05-27 15:40:08 -0300

    Detalhes

--- app/controllers/administracao_controller.rb
+++ app/controllers/administracao_controller.rb
@@ -52,6 +52,7 @@ def editar_funcionario
 end
 
 def salvar_funcionario
+ funcionario_params.delete(:local_nome)
  @funcionario = Funcionario.new(funcionario_params)
  @titulo = "Cadastrar Funcionário"
  @caminho = administracao_salvar_funcionario_path
@@ -85,7 +86,7 @@ def detalhes_funcionario
   @funcionario = Funcionario.find(params[:funcionario_id])
 end
 def funcionario_params
-  params.require(:funcionario).permit([:local_id,:local_nome,:documento,:usuario_id,:componente_curricular,:nome, :cpf, :cadastro, :classe, :padrao, :turmas, :carga_horaria, :ambiente,:ambiente_nao_docente, :formacao, :ch_em_sala, :cargo, :quadro, :concurso, :area_concurso, :programa, :situacao, :local_id,:local, :disciplina_concurso, :disciplina_atuacao, :municipio_concurso])
+  params.require(:funcionario).permit([:documento,:usuario_id,:componente_curricular,:nome, :cpf, :cadastro, :classe, :padrao, :turmas, :carga_horaria, :ambiente,:ambiente_nao_docente, :formacao, :ch_em_sala, :cargo, :quadro, :concurso, :area_concurso, :programa, :situacao, :local_id,:local, :disciplina_concurso, :disciplina_atuacao, :municipio_concurso])
 end
 
 end
